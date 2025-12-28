89e4523290c1dab34fbc5fded32c9837f3f8d453
Samioul51 <akmsamioulislam51@gmail.com>
2025-12-29 01:50:00 +0600

    Slight enhancement

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -24,10 +24,6 @@ const Service = async ({ params }) => {
 
       <h1 className="text-3xl font-bold mb-4">{service_name}</h1>
 
-      <p className="text-gray-600 mb-3 font-medium">
-        Location: <span className="text-gray-800">{location || "N/A"}</span>
-      </p>
-
       <p className="text-gray-700 mb-6">{description}</p>
 
       <p className="text-xl font-semibold mb-6">
